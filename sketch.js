fbb37fa3a130a870a3333e4ff308ffa56ebf7e33
IAMSAM <chortletortle@gmail.com>
2015-06-01 14:50:35 -0400

    horribly strapped together but it might work

--- sketch.js
+++ sketch.js
@@ -43,9 +43,7 @@ var sketch = function(p) {
             changingURL = true;
             soundFile.stop();
             soundFile = p.loadSound(sound.stream_url + '?client_id=' + client_id, falseC());
-            if(soundFile.isLoaded()) play();
-
-
+            play();
         });
         
         
@@ -56,10 +54,16 @@ var sketch = function(p) {
     }
 
     var play = function(){
-        soundFile.play();
-        soundFile.rate(1);
-        amplitude = new p5.Amplitude();
-        fft = new p5.FFT();
+        if(soundFile.isLoaded()){
+            soundFile.play();
+            soundFile.rate(1);
+            amplitude = new p5.Amplitude();
+            fft = new p5.FFT(); 
+        } else {
+            play();
+            console.log("WOW");
+        }
+            
     }
 
     var dofft = function() {
