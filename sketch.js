0bbff0088f94c5ac7bbc9d987cf1946d6a15db10
IAMSAM <chortletortle@gmail.com>
2015-06-01 13:56:03 -0400

    huh

--- sketch.js
+++ sketch.js
@@ -16,12 +16,13 @@ var sketch = function(p) {
 
     p.preload = function() {
         soundFile = p.loadSound(sound.stream_url + '?client_id=' + client_id);
+        soundFile.play();
+        soundFile.rate(1);
         //soundFormats('ogg', 'mp3');
     };
 
     p.setup = function() {
-        soundFile.play();
-        soundFile.rate(1);
+        
 
         amplitude = new p5.Amplitude();
         fft = new p5.FFT();
@@ -43,9 +44,7 @@ var sketch = function(p) {
     var changeURL = function(newUrl){
         SC.get('/resolve.json', { url: newUrl }, function(data) {
             sound = data;
-            soundFile = p.loadSound(sound.stream_url + '?client_id=' + client_id);
-            soundFile.play();
-        soundFile.rate(1);
+            p.preload();
         });
         
     }
