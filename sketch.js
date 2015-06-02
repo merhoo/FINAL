b0f84ea40111dbb1a9808b41f2cf37459c5a1bbe
IAMSAM <chortletortle@gmail.com>
2015-06-02 06:09:55 -0400

    so we can present!

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ var maxx = 0;
 
 //yeah it works!
 var client_id = '3018af38063abcf6a38748e9ad55b455';
-var url = 'https://soundcloud.com/dirgethexvii/eden';
-var url2 = 'https://soundcloud.com/bedjams/maxo-mediumrare-bedjams-remix';
+var url = 'https://soundcloud.com/tennysonmusic/smother-with-mothica';
+var url2 = 'https://soundcloud.com/dirgethexvii/eden';
 var sound, soundFile, soundFileN;
 var changingURL = false;
 
@@ -33,7 +33,7 @@ var sketch = function(p) {
             numpressed = p.keyCode - 48;
         }
         if (p.keyCode == 32){
-            //changeURL(url2);
+            changeURL(url2);
         }
     };
 
@@ -284,11 +284,11 @@ SC.get('/resolve.json', { url: url }, function(data) {
     sound = data;
     myp5 = new p5(sketch);
 });
-
+/*
 $(document).ready(function() {
 
     $('#songSubmit').click(function() {
         localSketch.changeURL($('#songURL').value());
     });
 
-});
+});*/
